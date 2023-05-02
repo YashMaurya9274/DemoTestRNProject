1e97c9cc20ec633cab467b8160d607e880cd2377
Yash Maurya <yash.maurya537468@gmail.com>
2023-05-02 13:13:02 +0530

    sliding cards flatlist animation

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import {View} from 'react-native';
 import React from 'react';
 import ScrollableFlatlist from './src/components/ScrollableFlatlist';
+import AnimatedTabsIndicator from './src/components/AnimatedTabsIndicator';
 
 const App = () => {
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
-      <ScrollableFlatlist />
+      {/* <ScrollableFlatlist /> */}
+      <AnimatedTabsIndicator />
     </View>
   );
 };
